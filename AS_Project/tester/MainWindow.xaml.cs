abf5ef8c7ae0368d790df6815de2a3c67e3e4f3f
iSketchup <kalulu@gmx.at>
2025-05-21 14:59:44 +0200

    hintergrund:()

--- AS_Project/tester/MainWindow.xaml.cs
+++ AS_Project/tester/MainWindow.xaml.cs
@@ -1,8 +1,7 @@
-﻿using AS;
+﻿
 using AsClass;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -10,6 +9,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Controls;
+using System.Windows.Media.Media3D;
 
 
 namespace tester
@@ -19,14 +21,64 @@ namespace tester
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        public WriteableBitmap wb = new WriteableBitmap(100, 100, 96, 96, PixelFormats.Bgra32, null);
+
         public MainWindow()
         {
             InitializeComponent();
 
-            AsClass.Frame y  = new AsClass.Frame(100, 100, 1);
+            ZeigeRotesBild((byte)beep.Value);
+            img.Source = wb;
+        }
 
-            x.Children.Add(y.img);
+        void ZeigeRotesBild(byte beep)
+        {
+            int stritde = wb.BackBufferStride;
+
+            wb.Lock();
+
+            unsafe
+            {
+                byte* buffer = (byte*)wb.BackBuffer;
+
+                for (int y = 0; y < 100; y++)
+                {
+                    for (int x = 0; x < 100; x++)
+                    {
+
+                        byte* pixel = buffer + y * stritde + x * 4;
+                        pixel[3] = beep;
+
+                            if (((x / 10) + (y / 10)) % 2 == 0)
+                        {
+                            pixel[0] = 153;
+                            pixel[1] = 153;
+                            pixel[2] = 153;
+                            }
+                        else
+                        {
+                            pixel[0] = 204;
+                            pixel[1] = 204;
+                            pixel[2] = 204;
+                        }
+
+
+                            
+                            }
+                    }
+
+            wb.AddDirtyRect(new Int32Rect(0, 0, 100, 100));
+
+
+
+                    }
+            wb.Unlock();
+        }
+
+        private void beep_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+            ZeigeRotesBild((byte)beep.Value);
         }
     }
 }
