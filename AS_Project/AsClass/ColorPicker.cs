b4a080001f878cbc363e85c354bf9e64f147cb93
COOKIE <dominiknageler343@gmail.com>
2025-06-03 08:59:56 +0200

    Colorpicker WIP

--- AS_Project/AsClass/ColorPicker.cs
+++ AS_Project/AsClass/ColorPicker.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 
 namespace AsClass
 {
@@ -22,11 +24,44 @@ namespace AsClass
             }
         }
 
-        public ColorPicker(WriteableBitmap wb)
+        public ColorPicker( double FrameHeight, double Framewidth)
         {
-           
+          
         }
 
+
+        public void Pixelmade(int x, int y,WriteableBitmap wb)
+        {
+            int stride = wb.BackBufferStride;
+
+            wb.Lock();
+
+            unsafe
+            {
+                byte* buffer = (byte*)wb.BackBuffer;
+
+
+
+
+
+                byte* pixel = buffer + y * stride + x * 4;
+                pixel[3] = 255;
+
+
+                pixel[0] = 0;
+                pixel[1] = 0;
+                pixel[2] = 255;
+
+
+                wb.AddDirtyRect(new Int32Rect(0, 0,wb.PixelWidth,wb.PixelHeight));
+
+
+
+            }
+            wb.Unlock();
+
+            ;
+        }
 
     }
 }
